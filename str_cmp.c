762c08a21fd9b55d14703251d1a38573042e8226
eebbroo <majamibrahim@gmail.com>
2022-08-10 01:33:18 -0700

    Bug fix in strcmp file

--- str_cmp.c
+++ str_cmp.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
 /**
- * _strcmp - This compares two strings
+ * _strcmp - Function to compares two strings
+ *
  * @s1: This is the first string
  * @s2: This is the second string
  *
- * Return: An int, 0 if equal, +ve if 1 > 2
- * -ve if 1 < 2
+ * Return: return 0 successful comp.
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i = 0, j = 0;
 
-	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
-		i++;
+	while (i == 0)
+	{
+		if ((*(s1 + j) == '\0') && (*(s2 + j) == '\0'))
+			break;
+		i = *(s1 + j) - *(s2 + j);
+		j++;
+	}
 
-	if (*(s2 + i))
-		return (*(s1 + i) - *(s2 + i));
-	else
-		return (0);
+	return (i);
 }
