8107490edf7ac76907f7027e5f3ea753fdc2d7ff
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-08-11 06:49:24 -0700

    made changes to str_help

--- str_help.c
+++ str_help.c
@@ -27,17 +27,15 @@ int _strlen(char *s)
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
-	if (*s1 == *s2)
-		return (0);
+	int i = 0;
+
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+		i++;
+
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
 	else
-		return (*s1 < *s2 ? -1 : 1);
+		return (0);
 }
 
 /**
